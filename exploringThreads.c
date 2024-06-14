28dba7b7c856b99172e71a8dc1f51c3e3d981a8e
olaaroub <olaaroub@c3r2p2.1337.ma>
2024-06-14 17:52:38 +0100

    thread join directemen after creating it

--- exploringThreads.c
+++ exploringThreads.c
@@ -6,7 +6,7 @@
 /*   By: olaaroub <olaaroub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 16:47:27 by olaaroub          #+#    #+#             */
-/*   Updated: 2024/06/13 17:13:28 by olaaroub         ###   ########.fr       */
+/*   Updated: 2024/06/14 15:00:33 by olaaroub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ int i = 7;
 
 void* testing2()
 {
-    printf("I GOT HERE TOO %d \n", i++);
+    i++;
+    printf("I GOT HERE TOO %d \n", i);
 }
 
 void*   testing()
@@ -25,14 +26,13 @@ void*   testing()
     printf("I GOT HERE %d \n", i);
 }
 
-
 int main(void)
 {
     pthread_t thread1, thread2;
 
     pthread_create(&thread1, NULL, &testing, NULL);
-    pthread_create(&thread2, NULL, &testing2, NULL);
-    
     pthread_join(thread1, NULL);
+    
+    pthread_create(&thread2, NULL, &testing2, NULL);
     pthread_join(thread2, NULL);
 }
