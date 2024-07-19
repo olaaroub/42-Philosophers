10757823a074bef6e2e6c298587d98a7c9b9dc11
olaaroub <olaaroub@c4r3p4.1337.ma>
2024-07-19 15:41:38 +0100

    going to executive center

--- inc/philo.h
+++ inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: olaaroub <olaaroub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 18:01:34 by olaaroub          #+#    #+#             */
-/*   Updated: 2024/07/19 11:53:34 by olaaroub         ###   ########.fr       */
+/*   Updated: 2024/07/19 15:05:34 by olaaroub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,18 @@
 #include <sys/time.h>
 #include <limits.h>
 
+
+typedef struct s_philo
+{
+    int id;
+    struct s_program   program;
+}   t_philo;
+
+
+typedef struct s_program
+{
+    t_philo     philo;
+}   t_program;
 
 
 int	    ft_isdigit(int c);
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: olaaroub <olaaroub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 18:00:53 by olaaroub          #+#    #+#             */
-/*   Updated: 2024/07/19 11:50:39 by olaaroub         ###   ########.fr       */
+/*   Updated: 2024/07/19 15:15:13 by olaaroub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,14 @@
 
 int main(int ac, char **av)
 {
+    t_program   data;
+    
     if(ac == 6 || ac == 5)
     {
         check_args(av, ac);
+        
     }
+    else
+        exit_when_error("Invalid arguments !\n");
     return 0;        
 }
